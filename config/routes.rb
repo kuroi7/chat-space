470894f6a48370170a5753dcf7b625900a44b0d1
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2020-03-31 14:48:10 +0900

    incremental-search
    インクリメンタルサーチを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 end
