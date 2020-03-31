c394b351431db5dadc60906dd636dedbdec5552f
kuroi7 <62415320+kuroi7@users.noreply.github.com>
2020-03-31 12:34:55 +0900

    add new file time_formats.rb
    time_formats.rbを新規追加

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,4 @@
+Date::DATE_FORMATS[:default] = "%Y年%m月%d日"
+Date::DATE_FORMATS[:date] = "%m/%d"
+DateTime::DATE_FORMATS[:default] = "%Y年%m月%d日 %H時%M分"
+DateTime::DATE_FORMATS[:datetime] = "%Y/%m/%d %H:%M"
